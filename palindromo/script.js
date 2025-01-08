15ffc019b48cd2f4b20f09e49bd1525c01af9ff8
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-08 16:13:44 +0100

    [feat] human code palindroma

---
+++ palindromo/script.js
@@ -0,0 +1,39 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiediamo all utente di inserire una parola e salviamo la variabile
+let userWord = prompt("inserisci una parola");
+
+console.log(userWord);
+console.log(palword(userWord));
+
+
+
+
+
+// per fare cio abbiamo bisogno di una funzione che ci identifica se la parola ha proprietà palindroma
+    // dentro la funzione dobbiamo invertire la parola
+        //se la parola che abbiamo è uguale alla parola invertita
+            // il risultato è:la parola è palindroma 
+            // return 'è palindroma'
+        // altrimenti 
+            //il risultato è: la parola non è palindroma
+            // return 'non è palindroma'
+        
+    
+
+
+
+
+
+
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// Domande da farsi quando si crea una funzione:
+// 1. Come dovrebbe chiamarsi?
+// 2. Ho bisogno di parametri?
+// 3. Devo restituire un valore?
+// 4. Se sì, di che tipo?
