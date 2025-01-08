7b96e325659bd48f1e322b4aefcaa269300a4648
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-08 16:47:02 +0100

    [feat] humancode pariodispari

---
+++ pariodispari/script.js
@@ -0,0 +1,45 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+// chiediamo all utente di inserire una numero da 1 a 5 e salviamo la variabile
+let userChoice = prompt("pari o dispari");
+let userNumber = parseInt(prompt("inserisci una numero da 1 a 5"));
+
+console.log("L'utente ha scelto:", userChoice, 'e il numero:', userNumber);
+
+// creiamo una funzione per generare un numero casuale da parte del computer che va da un minimo di 1 ad un massimo di 5
+
+// sommiamo il valore dell'utente con quello del computer
+
+// creiamo una funzione per vedere se il risultato è pari o dispari
+    // se il risultato è divisibile per 2 
+        // ritorna pari
+    // altrimenti
+        // ritorna dispari
+
+// se la scelte dell'utente è uguale a quella uscita dalla funzione l'utente ha vinto
+// altrimenti ha vinto il computer
+
+
+
+
+
+
+
+
+
+
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// Domande da farsi quando si crea una funzione:
+// 1. Come dovrebbe chiamarsi?
+// 2. Ho bisogno di parametri?
+// 3. Devo restituire un valore?
+// 4. Se sì, di che tipo?
