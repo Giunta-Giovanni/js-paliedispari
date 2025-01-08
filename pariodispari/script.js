65c51c672dbe404efe9b5652535e424da8a1415a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-08 17:44:12 +0100

    [feat] funzione pari o dispari

--- pariodispari/script.js
+++ pariodispari/script.js
@@ -6,26 +6,59 @@
 // Dichiariamo chi ha vinto.
 
 
-
 // chiediamo all utente di inserire una numero da 1 a 5 e salviamo la variabile
 let userChoice = prompt("pari o dispari");
 let userNumber = parseInt(prompt("inserisci una numero da 1 a 5"));
 
-console.log("L'utente ha scelto:", userChoice, 'e il numero:', userNumber);
-
-// creiamo una funzione per generare un numero casuale da parte del computer che va da un minimo di 1 ad un massimo di 5
+// stampiamo i dati user
+console.log("L'utente ha scelto:", userChoice); //valore
+console.log("L'utente ha scelto il numero:", userNumber); // numero
 
 // sommiamo il valore dell'utente con quello del computer
+let somma = userNumber + randomNum1to5();
 
-// creiamo una funzione per vedere se il risultato è pari o dispari
-    // se il risultato è divisibile per 2 
-        // ritorna pari
-    // altrimenti
-        // ritorna dispari
+console.log('la somma è:', somma); //stampiamo il numero della somma
+console.log('è uscito:', pariodispari(somma)); //stampiamo il valore della somma
 
 // se la scelte dell'utente è uguale a quella uscita dalla funzione l'utente ha vinto
-// altrimenti ha vinto il computer
+if (userChoice === pariodispari(somma)) {
+    console.log("L'utente ha vinto");
 
+// altrimenti ha vinto il computer
+} else {
+    console.log("Il computer ha vinto"); 
+}
+
+
+
+
+/********************* FUNZIONI ********************/  
+
+// creiamo una funzione per generare un numero casuale da parte del computer che va da un minimo di 1 ad un massimo di 5
+function randomNum1to5(){
+    const numeroGenerato = Math.floor(Math.random() * 5) + 1;
+    console.log('il computer ha scelto:', numeroGenerato);
+    return numeroGenerato;
+}
+
+// creiamo una funzione per vedere se il risultato è pari o dispari
+function pariodispari(numero) {
+
+    let risultato = '';
+    // se il risultato è divisibile per 2 
+    if (numero % 2 === 0) {
+        // modifica la variabile risultato con pari
+        risultato = 'pari';
+        // ritorna il risultato
+    } else {
+        // modifica la variabile risultato con dispari
+        risultato = 'dispari';
+        // ritorna il risultato
+    }
+return risultato
+}
+
+/***************************************************/ 
 
 
 
