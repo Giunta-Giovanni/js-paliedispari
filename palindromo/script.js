af1ff5db0d3a6ed5faf14a12fb51d22fa24f289d
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-08 16:35:20 +0100

    [feat] funzione palindroma

--- palindromo/script.js
+++ palindromo/script.js
@@ -5,28 +5,38 @@
 // chiediamo all utente di inserire una parola e salviamo la variabile
 let userWord = prompt("inserisci una parola");
 
-console.log(userWord);
-console.log(palword(userWord));
-
-
-
+console.log('la parola è:', userWord);
+console.log(palWord(userWord));
 
 
 // per fare cio abbiamo bisogno di una funzione che ci identifica se la parola ha proprietà palindroma
-    // dentro la funzione dobbiamo invertire la parola
-        //se la parola che abbiamo è uguale alla parola invertita
-            // il risultato è:la parola è palindroma 
-            // return 'è palindroma'
-        // altrimenti 
-            //il risultato è: la parola non è palindroma
-            // return 'non è palindroma'
-        
+function palWord(parola){
+    // per fare cio abbiamo bisogno di constatare che la parola che abbiamo è uguale alla parola invertita
+    // step 1 ci salviamo le variabili
+    let parolaRevertata = '';
+
+    // step 2 cicliamo la stringa dalla fine, all'inzio andando a prenderci ad ogni iterazione il carattere iesimo
+    for (let i = parola.length - 1; i >= 0; i--){ //(parti dalla fine; finiscili il ciclo quando arrivi a 0; andando indietro )
+        let carattereIesimo = parola[i];
     
+        // aggiungiamo alla variabile di parolaRevertata il carattere iesimo
+        parolaRevertata += carattereIesimo;
+    }
 
+    console.log('la parola letta al contrario è;', parolaRevertata);
+    
+        //se la parola che abbiamo è uguale alla parola invertita
+        if (parola === parolaRevertata) {
+            // il risultato è:la parola è palindroma 
+            return 'la parola inserita è palindroma' 
 
+        }else
+            //il risultato è: la parola non è palindroma
+            return 'la parola inserita non è palindroma' 
 
+}
 
-
+        
 
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
